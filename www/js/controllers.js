01bd3de096bfbe7c4b21ecd1bea6eb937ec40735
Twelver <lex.goryachev@gmail.com>
2014-10-10 00:16:22 +0400

    labels was changed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -35,12 +35,12 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Заявка 1', id: 1 },
+    { title: 'Заявка 2', id: 2 },
+    { title: 'Заявка 3', id: 3 },
+    { title: 'Заявка 4', id: 4 },
+    { title: 'Заявка 5', id: 5 },
+    { title: 'Заявка 6', id: 6 }
   ];
 })
 
